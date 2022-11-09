eb46a62b6a16f1aee94e9d05d989f1c40fc7655e
Ankur <86702313+ankuraaryan5@users.noreply.github.com>
2022-11-09 10:01:41 +0530

    Create sumoftwo.cpp

---
+++ sumoftwo.cpp
@@ -0,0 +1,12 @@
+// Write a program in C++ to print the sum of two numbers.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"enter two numbers ";
+    cin>>a>>b;
+    int sum = a+b;
+    cout<<"sum of "<< a<< " and "<< b << " is = "<< sum;
+}
