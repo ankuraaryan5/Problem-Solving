e77b03dab11247a83085ee2cb052188f3f12f92b
ankuraaryan5 <86702313+ankuraaryan5@users.noreply.github.com>
2022-11-09 19:54:28 +0530

    Create swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,17 @@
+// swapping two numbers
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"a: ";
+    cin>>a;
+    cout<<"b: ";
+    cin>>b;
+
+    c=b;
+    b=a;
+    a=c;
+    cout<<"a = "<<a <<endl << "b = "<<b;
+
+
+}
