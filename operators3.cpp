f9dfdd27afb755cb38c53b057cff0a88de7c979c
ankuraaryan5 <86702313+ankuraaryan5@users.noreply.github.com>
2022-11-13 21:28:42 +0530

    Create operators3.cpp

---
+++ operators3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //miscellanous operator
+    float a = 3.21; // = is called assignment operator
+    cout<<a<<endl;
+    cout<<(int)a<<endl; // this is called casting
+    //conditional operator
+    int b= 32, c=65;
+    cout<<"max of b and c is "<<((b>c)?b:c)<<endl;
+}
